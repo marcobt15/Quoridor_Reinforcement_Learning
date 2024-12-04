5d0605dd2aa8a39ead8d49a26339aa4c66dfb046
noahbreton8 <noahbreton8@gmail.com>
2024-12-04 15:06:35 -0500

    add new truncation rewards

--- quoridor.py
+++ quoridor.py
@@ -238,22 +238,45 @@ class Quoridor(AECEnv):
 
             # Penalize others
             self.rewards[opponent] = -100
-
-        # if they take too long then give -1 reward
+            
         elif self.truncations[current_agent]:
             # pass
-            self.rewards = {agent: -1000 for agent in self.agents}
+            curr_row = self.player_positions[current_agent][0] if current_agent == "player_1" else 8-self.player_positions[opponent][0]
+            rew = 0
+            if curr_row == 0:
+                rew = -1000
+            elif curr_row == 1:
+                rew = -1000
+            elif curr_row == 2:
+                rew = -500
+            elif curr_row == 3:
+                rew = -400
+            elif curr_row == 4:
+                rew = -200
+            elif curr_row == 5:
+                rew = 25
+            elif curr_row == 6:
+                rew = 50
+            elif curr_row == 7:
+                rew = 100
+
+            self.rewards[current_agent] = rew
+            self.rewards[opponent] = -1000
+        #if they take too long then give -1 reward
+        # elif self.truncations[current_agent]:
+        #     # pass
+        #     self.rewards = {agent: -1000 for agent in self.agents}
 
         else: #not terminated or truncated
             if action < 4:
-                curr_reward = (pre_cost > post_cost) if pre_cost > post_cost else -1
+                curr_reward = 3*(pre_cost > post_cost) if pre_cost > post_cost else -1
             #just not passing api test and i don't know what to do to fix it
             if action < 8 and action >= 4:
                 #best path doesn't involve jumping so if they jump it should reduce the path cost by more than one getting higher reward
                 curr_reward = 1 if pre_cost > post_cost else 0
             else:
                 #the more they block their opponent the better the reward
-                curr_reward = 6*(post_opp_cost-pre_opp_cost) if pre_opp_cost < post_opp_cost else 0
+                curr_reward = 5*(post_opp_cost-pre_opp_cost) if pre_opp_cost < post_opp_cost else 0
                 # curr_reward = 0   
 
                 curr_reward = 0
